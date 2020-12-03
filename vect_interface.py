913a4529ff1e5c9a451e825beee340a2e8710d59
Dashaless3 <72004269+Dashaless3@users.noreply.github.com>
2020-12-03 13:21:28 +0300

    Незначительная поправка
    Функция create_electro_vectors теперь принимает на один аргумент меньше

--- vect_interface.py
+++ vect_interface.py
@@ -17,25 +17,29 @@ class Mouse():
         self.x = event.x
         self.y = event.y
 
-def color_vector(max_length, k, vectors):
-    x1 = vectors[k][0][0]
-    y1 = vectors[k][1][0]
-    x2 = vectors[k][0][1]
-    y2 = vectors[k][1][1]
-    red = int((((x1 - x2)**2 + (y1 - y2)**2)**(1/2)/max_length)*255)
-    blue = int((1 - ((x1 - x2)**2 + (y1 - y2)**2)**(1/2)/max_length)*255)
-    color = '#' + hex(red)[2:].ljust(2, '0') + '00' + hex(blue)[2:].ljust(2, '0')
-    return color
+def sigm(x):
+    return 1 / (1 + 1.055 ** (-x*400))
 
-def create_electro_vectors(step, vectors, electro):
+def create_electro_vectors(vectors, electro):
+    global C
     for k in range (len(vectors)):
         pointx1 = vectors[k][0][0] + window_width/2 + 15
         pointy1 = vectors[k][1][0] + window_height/2 + 15
         pointx2 = vectors[k][0][1] + window_width/2 + 15
         pointy2 = vectors[k][1][1] + window_height/2 + 15
-        color = color_vector(step, k, vectors)
-        electro.create_line(pointx1, pointy1, pointx2, pointy2, width = 5, fill = color)
-
+        color = '#' + (hex(int(sigm(vectors[k][2])*255))[2:].ljust(2, '0') + '1a' +
+                 hex(int((1 - sigm(vectors[k][2]))*204))[2:].ljust(2, '0'))
+        electro.create_line(pointx1, pointy1, pointx2, pointy2, width = 1, fill = color)
+        '''стрелочка на конце'''
+        length = dist_point_to_point(pointx1, pointy1, pointx2, pointy2)
+        A, B = quotients(pointx1, pointy1, pointx2, pointy2)
+        C_new = B*(pointx1 + 3*pointx2)/4 - A*(pointy1 + 3*pointy2)/4
+        x_a = A*length/(8*(A**2+B**2)**(1/2)) + (B*C_new - A*C)/(A**2+B**2)
+        y_a = (B*x_a - C_new)/A
+        x_b = -A*length/(8*(A**2+B**2)**(1/2)) + (B*C_new - A*C)/(A**2+B**2)
+        y_b = (B*x_b - C_new)/A
+        electro.create_polygon([pointx2, pointy2, x_a, y_a, x_b, y_b], width = 1, fill = color)
+        
 def create_points(mouse, con_working, points, electro, magnet):
     for k in range (len(points)):
         x = mouse.x
@@ -68,15 +72,22 @@ def create_lines_between_points(con_working, points, bonds, x, y, electro, magne
             magnet.create_line(pointx1, pointy1, x, y)
 
 def dist_mouse_to_line(k, points, bonds, x, y):
-    global C
     x1 = points[bonds[k][0]][0] + window_width/2 + 15
     y1 = points[bonds[k][0]][1] + window_height/2 + 15
     x2 = points[bonds[k][1]][0] + window_width/2 + 15
     y2 = points[bonds[k][1]][1] + window_height/2 + 15
-    B = C *(x1 - x2) / (y1*x2 - y2*x1)
-    A = - (C + B*y1) / x1
+    A, B = quotients(x1, y1, x2, y2)
     dist = abs(A*x + B*y + C) / (A**2 + B**2)**(1/2)
     return dist
+
+def dist_point_to_point(x1, y1, x2, y2):
+    return ((x2 - x1)**2 + (y2 - y1)**2)**(1/2)
+
+def quotients(x1, y1, x2, y2):
+    global C
+    B = C *(x1 - x2) / (y1*x2 - y2*x1)
+    A = - (C + B*y1) / x1
+    return A, B
 
 def highlight_lines_between_points(del_working, points, bonds, x, y, electro, magnet):
     for k in range (len(bonds)):

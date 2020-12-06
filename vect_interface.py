75235cb0825d9c61a464d5ca23792d04aeef46b0
Dashaless3 <72004269+Dashaless3@users.noreply.github.com>
2020-12-06 18:13:59 +0300

    Добавлена функция изменения масштаба
    И изменен размер точек

--- vect_interface.py
+++ vect_interface.py
@@ -1,8 +1,6 @@
 from tkinter import *
 
 # CONSTANTS
-window_width = 610
-window_height = 610
 sense = 7 #дальность обнаружения мыши для точки
 max_dist = 5 #дальность обнаружения мыши для прямой
 '''В функции dist_point_to_line будет использована функция расчета 
@@ -20,16 +18,19 @@ class Mouse():
 def sigm(x):
     return 1 / (1 + 1.055 ** (-x*400))
 
-def scale(x):
-    return x + window_width/2 + 15
+def scale_x(x, window_settings):
+    return x*window_settings[0]/(2*window_settings[2]) + window_settings[0]/2 + 5
 
-def create_vectors(vectors, screen):
+def scale_y(x, window_settings):
+    return x*window_settings[1]/(2*window_settings[3]) + window_settings[1]/2 + 5
+
+def create_vectors(vectors, window_settings, screen):
     global C
     for k in range (len(vectors)):
-        pointx1 = scale(vectors[k][0][0])
-        pointy1 = scale(vectors[k][1][0])
-        pointx2 = scale(vectors[k][0][1])
-        pointy2 = scale(vectors[k][1][1])
+        pointx1 = scale_x(vectors[k][0][0], window_settings)
+        pointy1 = scale_y(vectors[k][1][0], window_settings)
+        pointx2 = scale_x(vectors[k][0][1], window_settings)
+        pointy2 = scale_y(vectors[k][1][1], window_settings)
         color = '#' + (hex(int(sigm(vectors[k][2])*255))[2:].ljust(2, '0') + '1a' +
                        hex(int((1 - sigm(vectors[k][2]))*204))[2:].ljust(2, '0'))
         screen.create_line(pointx1, pointy1, pointx2, pointy2, width = 1, fill = color)
@@ -44,14 +45,15 @@ def create_vectors(vectors, screen):
             y_b = (B*x_b - C_new)/A
             screen.create_polygon([pointx2, pointy2, x_a, y_a, x_b, y_b], width = 1, fill = color)
         
-def create_points(mouse, con_working, points, screen):
+def create_points(mouse, con_working, points, window_settings,
+                  screen):
     for k in range (len(points)):
         x = mouse.x
         y = mouse.y
-        pointx = scale(points[k][0])
-        pointy = scale(points[k][1])
-        screen.create_oval(pointx - 2, pointy - 2, pointx + 2,
-                            pointy + 1, fill="orange")
+        pointx = scale_x(points[k][0], window_settings)
+        pointy = scale_y(points[k][1], window_settings)
+        screen.create_oval(pointx - 4, pointy - 4, pointx + 4,
+                            pointy + 4, fill="orange")
         if (x - pointx)**2 + (y - pointy)**2 < sense**2 and con_working:
             screen.create_oval(pointx - sense, pointy - sense, pointx + sense,
                                 pointy + sense, fill="red")
@@ -78,24 +80,25 @@ def create_first_bonds(points, Links):
                 bonds.append([i, j])
     return bonds
             
-def create_lines_between_points(con_working, points, bonds, x, y, screen):
+def create_lines_between_points(con_working, points, bonds, x, y,
+                                window_settings, screen):
     for k in range (len(bonds)):
         if len(bonds[k]) == 2:
-            pointx1 = scale(points[bonds[k][0]][0])
-            pointy1 = scale(points[bonds[k][0]][1])
-            pointx2 = scale(points[bonds[k][1]][0])
-            pointy2 = scale(points[bonds[k][1]][1])
+            pointx1 = scale_x(points[bonds[k][0]][0], window_settings)
+            pointy1 = scale_y(points[bonds[k][0]][1], window_settings)
+            pointx2 = scale_x(points[bonds[k][1]][0], window_settings)
+            pointy2 = scale_y(points[bonds[k][1]][1], window_settings)
             screen.create_line(pointx1, pointy1, pointx2, pointy2)
         elif len(bonds[k]) == 1 and con_working:
-            pointx1 = scale(points[bonds[k][0]][0])
-            pointy1 = scale(points[bonds[k][0]][1])
+            pointx1 = scale_x(points[bonds[k][0]][0], window_settings)
+            pointy1 = scale_y(points[bonds[k][0]][1], window_settings)
             screen.create_line(pointx1, pointy1, x, y)
 
-def dist_mouse_to_line(k, points, bonds, x, y):
-    x1 = scale(points[bonds[k][0]][0])
-    y1 = scale(points[bonds[k][0]][1])
-    x2 = scale(points[bonds[k][1]][0])
-    y2 = scale(points[bonds[k][1]][1])
+def dist_mouse_to_line(k, points, bonds, x, y, window_settings):
+    x1 = scale_x(points[bonds[k][0]][0], window_settings)
+    y1 = scale_y(points[bonds[k][0]][1], window_settings)
+    x2 = scale_x(points[bonds[k][1]][0], window_settings)
+    y2 = scale_y(points[bonds[k][1]][1], window_settings)
     A, B = quotients(x1, y1, x2, y2)
     dist = abs(A*x + B*y + C) / (A**2 + B**2)**(1/2)
     return dist
@@ -109,15 +112,16 @@ def quotients(x1, y1, x2, y2):
     A = - (C + B*y1) / x1
     return A, B
 
-def highlight_lines_between_points(del_working, points, bonds, x, y, screen):
+def highlight_lines_between_points(del_working, points, bonds, x, y,
+                                   window_settings, screen):
     for k in range (len(bonds)):
         if len(bonds[k]) == 2 and del_working:
-            dist = dist_mouse_to_line(k, points, bonds, x, y)
+            dist = dist_mouse_to_line(k, points, bonds, x, y, window_settings)
             if dist < max_dist:
-                pointx1 = scale(points[bonds[k][0]][0])
-                pointy1 = scale(points[bonds[k][0]][1])
-                pointx2 = scale(points[bonds[k][1]][0])
-                pointy2 = scale(points[bonds[k][1]][1])
+                pointx1 = scale_x(points[bonds[k][0]][0], window_settings)
+                pointy1 = scale_y(points[bonds[k][0]][1], window_settings)
+                pointx2 = scale_x(points[bonds[k][1]][0], window_settings)
+                pointy2 = scale_y(points[bonds[k][1]][1], window_settings)
                 if (x - pointx1)**2 + (y - pointy1)**2 < max_dist**2 + ((pointx1 - pointx2)**2 + (pointy1 - pointy2)**2)/2 or (
                     (x - pointx2)**2 + (y - pointy2)**2 < max_dist**2 + ((pointx1 - pointx2)**2 + (pointy1 - pointy2)**2)/2):
                     screen.create_line(pointx1, pointy1, pointx2, pointy2, fill = 'blue', width = 5)
